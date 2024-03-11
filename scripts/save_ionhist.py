b4deca0ae66c39fc7816c2c26556856a2abcbe4a
Lisa McBride <lisaleemcb@gmail.com>
2024-03-11 11:30:43 +0100

    adjusted save_ionhist file to use ref file for sim numbers

--- scripts/save_ionhist.py
+++ scripts/save_ionhist.py
@@ -3,15 +3,17 @@ import numpy as np
 
 import catwoman.cat as cat
 
-sims_num = []
-for filename in os.listdir('/Users/emcbride/kSZ/data/Pee_spectra_LoReLi/formatted/'):
-    basename, extension = os.path.splitext(filename)
-    sim, num = basename.split('u')
 
-    sims_num.append(num)
+
+# sims_num = []
+# for filename in os.listdir('/Users/emcbride/kSZ/data/Pee_spectra_LoReLi/formatted/'):
+#     basename, extension = os.path.splitext(filename)
+#     sim, num = basename.split('u')
+
+#     sims_num.append(num)
 
 ion_histories = {}
-for sn in sims_num:
+for sn in open("/obs/emcbride/catwoman/refs/sim_nums.txt",'r'):
     sim = cat.Cat(sn,
                     verbose=False,
                     load_ion=True,
