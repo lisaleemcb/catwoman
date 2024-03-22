5f3bc41f16e4e9fdc13071dff06fda003b7eaa84
Mc Bride Elizabeth <emcbride@mesopsl.obspm.fr>
2024-03-22 16:44:02 +0100

    changes to ion hist script

--- src/catwoman/cat.py
+++ src/catwoman/cat.py
@@ -115,7 +115,7 @@ class Cat:
     def fetch_redshifts(self):
         if self.verbose:
             print('Fetching redshifts...')
-        fn_z = f'{self.path_params}/redshift_list.dat'
+        fn_z = f'{self.path_sim}/simu{self.sim_n}/postprocessing/cubes/lum/redshift_list.dat'
 
         redshifts = {}
         with open(fn_z) as f:
