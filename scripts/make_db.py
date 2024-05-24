84074e298d624266b543816a128ddd46cc40750f
Lisa McBride <lisaleemcb@gmail.com>
2024-05-24 11:38:31 +0200

    debugging

--- scripts/make_db.py
+++ scripts/make_db.py
@@ -124,14 +124,16 @@ for sn in sims_num:
                 zf = np.where(sim.xe > .9)[0][0] + 1
                 zrange = (z0, zf)
 
+                Pdd = Pdd[z0:zf,k0:kf]
+
                 truths = [np.log10(modelparams_Gorce2022['alpha_0']), modelparams_Gorce2022['kappa']]
                 priors =[(np.log10(modelparams_Gorce2022['alpha_0']) * .75, np.log10(modelparams_Gorce2022['alpha_0']) * 1.25),
                          (0, modelparams_Gorce2022['kappa'] * 5.0),
                          (modelparams_Gorce2022['k_f'] * .25, modelparams_Gorce2022['k_f'] * 5.0),
                          (modelparams_Gorce2022['g'] * .25, modelparams_Gorce2022['g'] * 5.0)]
 
-                fit2 = ksz.analyse.Fit((z0, zf), (k0, kf), modelparams_Gorce2022, sim, priors, Pdd=Pdd[z0:zf,k0:kf], ndim=2)
-                fit4 = ksz.analyse.Fit((z0, zf), (k0, kf), modelparams_Gorce2022, sim, priors, Pdd=Pdd[z0:zf,k0:kf], ndim=4, burnin=1000, nsteps=int(1e5))
+                fit2 = ksz.analyse.Fit(zrange, krange, modelparams_Gorce2022, sim, priors, Pdd=Pdd, ndim=2)
+                fit4 = ksz.analyse.Fit(zrange, krange, modelparams_Gorce2022, sim, priors, Pdd=Pdd, ndim=4, burnin=1000, nsteps=int(1e5))
 
                 fits2[sn] = fit2
                 fits4[sn] = fit4
